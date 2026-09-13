475b326db2356ff7ec5bd4999160ecc05a99c851
kloczi <michalk@lazelab.com>
2026-09-13 08:59:30 +0200

    chore: scaffold from module template

---
+++ README.md
@@ -0,0 +1,11 @@
+# entirius-django-communicator
+
+Channel-agnostic communication for the Volkanos platform: templates, review, sequences, send policy and inbound replies
+
+## Install
+
+```shell
+pip install entirius-django-communicator
+```
+
+Status: scaffold (0.1.0). See `AGENTS.md`.
---
+++ src/django_communicator/__init__.py
@@ -0,0 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""Channel-agnostic communication for the Volkanos platform: templates, review, sequences, send policy and inbound replies"""
+
+__version__ = "0.1.0"
